547710601283b664dd09a4245649d25f7c74d775
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-05 11:23:36 +0700

    Thêm hook xóa table sau khi chạy xong

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,11 @@
+{{
+    config(
+        post_hook=[
+          "drop table  {{ this }}"
+        ]
+    )
+    }}
+
 with source as (select *
                 from raw_customers),
      renamed as (select id as customer_id,
