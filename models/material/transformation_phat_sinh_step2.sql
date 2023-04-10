dc238a84d634de707c9c4bdeda5c47876d07c163
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 14:30:11 +0700

    Test for

--- models/material/transformation_phat_sinh_step2.sql
+++ models/material/transformation_phat_sinh_step2.sql
@@ -1,4 +1,5 @@
-    {% for i in (select tk
-            from tk_and_year_created) %}
+{% set tks = (select tk from tk_and_year_created) %}
+
+{% for i in tks %}
         select nam from tk_and_year_created where tk={{tk}} union all
     {% endfor %}
