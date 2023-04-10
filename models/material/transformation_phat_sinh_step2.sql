0b4ec75ada7cbd3c9ee61a1326953c702f3415fa
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 15:14:31 +0700

    Test for ex

--- models/material/transformation_phat_sinh_step2.sql
+++ models/material/transformation_phat_sinh_step2.sql
@@ -1,6 +1,6 @@
 {% set tks = geTks() %}
 
 {% for i in tks %}
-        select nam from transformation_phat_sinh_thang_1 where tk="{{i}}"
+        select nam from transformation_phat_sinh_thang_1 where tk='{{i}}'
         {% if not loop.last -%} union all {%- endif %}
     {% endfor %}
