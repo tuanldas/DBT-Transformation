5ad8f5bd7fcb77b5627919dd94e969e2aa680528
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 14:36:24 +0700

    Test for ex

--- models/material/transformation_phat_sinh_step2.sql
+++ models/material/transformation_phat_sinh_step2.sql
@@ -1,4 +1,4 @@
-{% set tks = (select tk from tk_and_year_created) %}
+{% set tks = select tk from tk_and_year_created %}
 
 {% for i in tks %}
         select nam from tk_and_year_created where tk={{tk}} union all
