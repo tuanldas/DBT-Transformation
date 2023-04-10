05355f980295eb7b2141a19b8782112a86e13d13
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 14:48:51 +0700

    Test for ex

--- macros/get_tk.sql
+++ macros/get_tk.sql
@@ -1,6 +1,3 @@
 {% macro geTks() %}
-{{ return(select tk
-                             from "core.so_du_dau_ky_moi_tai_khoan"
-                             group by tk
-                             order by tk) }}
+{{ return(select tk from "core.so_du_dau_ky_moi_tai_khoan" group by tk order by tk) }}
 {% endmacro %}
