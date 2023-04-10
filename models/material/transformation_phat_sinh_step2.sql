1b2faa4d9d257f154a5759439eda9ebb75321af9
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 14:13:44 +0700

    Test for

--- models/material/transformation_phat_sinh_step2.sql
+++ models/material/transformation_phat_sinh_step2.sql
@@ -1,23 +1,4 @@
-with months as
-         (SELECT DISTINCT *
-          FROM (VALUES (1),
-                       (2),
-                       (3),
-                       (4),
-                       (5),
-                       (6),
-                       (7),
-                       (8),
-                       (9),
-                       (10),
-                       (11),
-                       (12)) AS x(month)),
-     tk_and_year_created as (select tk, min(nam)
-                             from "core.so_du_dau_ky_moi_tai_khoan"
-                             group by tk
-                             order by tk),
-     tk as (select tk
-            from tk_and_year_created)
-    {% for i in tk %}
+    {% for i in (select tk
+            from tk_and_year_created) %}
         select nam from tk_and_year_created where tk={{tk}} union all
     {% endfor %}
