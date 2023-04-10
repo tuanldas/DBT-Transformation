3f7df0e30946d0f708d64024506a9bc6643eca8e
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 14:47:36 +0700

    Test for ex

--- macros/get_tk.sql
+++ macros/get_tk.sql
@@ -1,3 +1,6 @@
 {% macro geTks() %}
-{{ return(select tk from transformation_phat_sinh_thang_1) }}
+{{ return(select tk
+                             from "core.so_du_dau_ky_moi_tai_khoan"
+                             group by tk
+                             order by tk) }}
 {% endmacro %}
