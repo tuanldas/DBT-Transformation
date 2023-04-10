6fe06053e2f89b3276c7507c98ebe9354aef7a81
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 14:54:00 +0700

    Test for ex

--- macros/get_tk.sql
+++ macros/get_tk.sql
@@ -1,3 +1,15 @@
 {% macro geTks() %}
-{{ return(select tk from "core.so_du_dau_ky_moi_tai_khoan" group by tk order by tk) }}
+
+{% set payment_methods_query %}
+    select tk from "core.so_du_dau_ky_moi_tai_khoan" group by tk order by tk
+{% endset %}
+{% set results = run_query(payment_methods_query) %}
+    {% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{{ return(results_list) }}
 {% endmacro %}
