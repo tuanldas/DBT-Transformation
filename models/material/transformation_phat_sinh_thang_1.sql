eac0054e6611ef6bbd0684795018cdeac81ec498
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-10 13:49:12 +0700

    git

---
+++ models/material/transformation_phat_sinh_thang_1.sql
@@ -0,0 +1,32 @@
+with months as
+         (SELECT DISTINCT *
+          FROM (VALUES (1),
+                       (2),
+                       (3),
+                       (4),
+                       (5),
+                       (6),
+                       (7),
+                       (8),
+                       (9),
+                       (10),
+                       (11),
+                       (12)) AS x(month)),
+     tk_and_year_created as (select tk, min(nam)
+                             from "core.so_du_dau_ky_moi_tai_khoan"
+                             group by tk
+                             order by tk)
+--      years as (SELECT DISTINCT *
+--                FROM (VALUES (2017),
+--                             (2018),
+--                             (2019),
+--                             (2020),
+--                             (2021),
+--                             (2022),
+--                             (2023)) AS x(year))
+-- select distinct month, year
+-- from months
+--          join years on true
+-- order by year, month;
+
+select * from tk_and_year_created
