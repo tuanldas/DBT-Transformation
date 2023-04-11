3045cb40503f97fcb713ab68a8ebb968f557a1fa
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-11 11:18:41 +0700

    Change name

--- models/material/transformation_phat_sinh_step2.sql
+++ models/material/transformation_phat_sinh_step2.sql
@@ -1,6 +1,6 @@
 {% set tks = geTks() %}
 
 {% for i in tks %}
-        select nam from transformation_phat_sinh_thang_1 where tk='{{i}}'
+        select nam from ghep_so_du_dau_ky_va_phat_sinh.sql where tk='{{i}}'
         {% if not loop.last -%} union all {%- endif %}
     {% endfor %}
