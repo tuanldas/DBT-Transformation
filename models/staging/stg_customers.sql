7dd16fb6a9bf5e02014da6f533870f7dcf8a24b7
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-17 13:29:53 +0700

    Custom schema

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,9 +1,3 @@
-
-{{ config(
-    schema=var("project")
-) }}
-
-
 with source as (select *
                 from {{ source('public', 'raw_customers')}}),
      renamed as (select id as customer_id,
