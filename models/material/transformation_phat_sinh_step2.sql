a027bc96cf5c017368544f07e8414698b0202b84
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-17 10:27:03 +0700

    Change dong_tien to dong_tien_trong_ky

--- models/material/transformation_phat_sinh_step2.sql
+++
@@ -1,6 +0,0 @@
-{% set tks = geTks() %}
-
-{% for i in tks %}
-        select nam from ghep_so_du_dau_ky_va_phat_sinh.sql where tk='{{i}}'
-        {% if not loop.last -%} union all {%- endif %}
-    {% endfor %}
