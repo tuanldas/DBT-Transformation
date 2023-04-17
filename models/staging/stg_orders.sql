fcbac4c63eaef9c508adff41603d560f7295152a
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-17 10:28:43 +0700

    Change dong_tien to dong_tien_trong_ky

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,5 @@
 with source as (select *
-                from {{ source('jaffle_shop', 'raw_orders')}}),
+                from {{ source('public', 'raw_orders')}}),
 
      renamed as (select id      as order_id,
                         user_id as customer_id,
